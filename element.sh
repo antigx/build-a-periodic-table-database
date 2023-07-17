c73454f96aa80a1d58ad64dae14caa0f15ec5f66
antigx <tiago.mamedio.barbosa@gmail.com>
2023-07-17 02:46:25 +0000

    fix: check of arguments

--- element.sh
+++ element.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-PSQL="psql -X --username=freecodecamp --dbname=periodic_table -t   -c"
+PSQL="psql -X --username=freecodecamp --dbname=periodic_table -t  --no-align -c"
 
 if [[ ! $1 ]]
 then
@@ -7,9 +7,11 @@ then
 else
   if [[ $1 =~ ^[0-9]*$ ]]
   then
-    echo "$($PSQL "select e.name, e.symbol, t.type, p.atomic_mass, p.melting_point_celsius, p.boiling_point_celsius from elements as e inner join properties as p using(atomic_number) inner join types as t using(type_id) where atomic_number=$1 OR symbol='$1' or name='$1'")" | while read NAME BAR SYMBOL BAR TYPE BAR MASS BAR MELT BAR BOIL
-    do
-      echo "The element with atomic number $ATOMIC is $NAME ($SYMBOL). It's a $TYPE, with a mass of $MASS amu. $NAME has a melting point of $MELT celsius and a boiling point of $BOIL celsius."
-    done
-  fi 
+    WHERE="where atomic_number=$1"
+  else
+      WHERE="WHERE symbol='$1' or name='$1'"
+  fi
+  RESULT="$($PSQL "select e.atomic_number, e.name, e.symbol, t.type, p.atomic_mass, p.melting_point_celsius, p.boiling_point_celsius from elements as e inner join properties as p using(atomic_number) inner join types as t using(type_id) $WHERE")"
+  IFS="|" read ATOMIC NAME SYMBOL TYPE MASS MELT BOIL <<< $RESULT
+  echo "The element with atomic number $ATOMIC is $NAME ($SYMBOL). It's a $TYPE, with a mass of $MASS amu. $NAME has a melting point of $MELT celsius and a boiling point of $BOIL celsius."
 fi
